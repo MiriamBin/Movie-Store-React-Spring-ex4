f07fba7250c266d44f8977e9024c191af046b775
Miriam Binyamines <89273650+MiriamBin@users.noreply.github.com>
2023-05-17 23:58:56 +0300

    Revert "same changes"
    This reverts commit b99e5aae1939578e0d29d88cbc9cc69472c93a09.

--- movie-app/src/components/MediaList.js
+++ movie-app/src/components/MediaList.js
@@ -4,20 +4,16 @@ import { Col, Row } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 
 function MediaList({ listMovies, cartItems, setCartItems }) {
-    // TODO: i don't understand for what this function is used
     if (!Array.isArray(listMovies)) {
         return null; // Return null or show an alternative component/error message when listMovies is not an array
     }
 
-
-
-
     return (
         <Container>
-            <Row className="justify-content-center g-lg-5 g-md-4 g-sm-3 ">
+            <Row>
                 {listMovies.map((movie, idx) => (
                     <Col key={idx}>
-                        <CardInfo movieData={movie}/>
+                        <CardInfo movieData={movie} cartItems={cartItems} setCartItems={setCartItems} />
                     </Col>
                 ))}
             </Row>
