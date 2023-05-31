d967c29600f3c551a318f6e01528cc3f7f301682
SalomonDemma <dennasa@edu.hac.ac.il>
2023-06-01 00:29:05 +0300

    added messages const and cleaning "SearchPage"

--- movie-app/src/components/constants/Messages.js
+++ movie-app/src/components/constants/Messages.js
@@ -1,2 +1,3 @@
 export const MSG_PAGE_NOT_FOUND = "Page not found!";
 export const MSG_EMPTY_CART = "Cart is empty! Go to shop and add some items!";
+export const FETCH_ERROR_MSG = 'Something went wrong...';
--- movie-app/src/components/pages/SearchPage.js
+++ movie-app/src/components/pages/SearchPage.js
@@ -1,14 +1,12 @@
 import React, {useState, useEffect} from 'react';
-import {Col, Row, Spinner} from 'react-bootstrap';
+import {Row, Spinner} from 'react-bootstrap';
 import MediaList from '../MediaList';
 import useFetch from '../hooks/useFetch';
 import {POPULAR_MOVIES_URL} from '../constants/ApiUrl';
 import FilterTabs from "../FilterTabs";
-
-
+import {FETCH_ERROR_MSG} from "../constants/Messages";
 
 function SearchPage() {
-    const FETCH_ERROR_MSG = 'Something went wrong...';
     const [queryParams, setQueryParams] = useState("");
     const [{ data: moviesData, isLoading: moviesIsLoading, isError: moviesIsError }, setMoviesUrl] = useFetch(
         POPULAR_MOVIES_URL + `&page=${1}`,
@@ -29,15 +27,7 @@ function SearchPage() {
             </Row>
             <Row className="justify-content-center m-4 row-cols-1">
                 {moviesIsError && <div className="alert alert-danger">{FETCH_ERROR_MSG}</div>}
-                {moviesIsLoading ? (
-                    <Row className="justify-content-center text-center">
-                        <Col>
-                            <Spinner animation="border" variant="light" />
-                        </Col>
-                    </Row>
-                ) : (
-                    <MediaList listMovies={moviesData.results} />
-                )}
+                {moviesIsLoading ? (<Spinner animation="border" variant="light" />) : (<MediaList listMovies={moviesData.results} />)}
             </Row>
         </>
     );
